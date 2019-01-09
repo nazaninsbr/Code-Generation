e2c9cc2efced5720a7508903c3ec1a14d071fb11
Marzieh <marziehrezaie2@gmail.com>
2019-01-09 17:03:56 +0330

    exception with array

--- java_codes/z13.java
+++ java_codes/z13.java
@@ -3,6 +3,13 @@ public class z13 {
 public static void main(String[] args) 
     { 
         int[] array = new int[4]; 
-        System.out.println(array.length); 
+        array[0] = 100;
+        array[1] = 150;
+        array[2] = 200;
+        array[3] = 300;
+        for (int i = 0; i < array.length; i++){
+        	System.out.println(array[i]);
+        }
+        
     } 
 }
